ed6bf54a361f9f5aec40deaef2f9b85c58ff04ae
liyo <liyo1242@gmail.com>
2022-06-30 16:45:30 +0800

    wip: setting about i18n

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { i18n } = require('./next-i18next.config')
+
 const nextConfig = {
   reactStrictMode: true,
-  styledComponents: true,
+  i18n,
 }
 
 module.exports = nextConfig
