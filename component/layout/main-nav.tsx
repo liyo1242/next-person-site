436e74d7ccd00d3a62df4247f1049a58894d446a
liyo <liyo1242@gmail.com>
2022-07-01 22:33:04 +0800

    feat: define home page content and layout

--- component/layout/main-nav.tsx
+++ component/layout/main-nav.tsx
@@ -1,17 +1,40 @@
 import Link from 'next/link'
 import classes from './main-nav.module.css'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+
+const navLinks = [
+  {
+    path: '/',
+    text: 'About me',
+  },
+  {
+    path: '/posts',
+    text: 'Post',
+  },
+]
 
 function MainNav() {
+  const router = useRouter()
+
+  const [activePath, setActivePath] = useState('')
+
+  useEffect(() => {
+    setActivePath('/' + (router.pathname.split('/')[1] || ''))
+  }, [router.pathname])
+
   return (
     <header className={classes.header}>
       <nav>
         <ul>
-          <li>
-            <Link href="/post">Posts</Link>
-          </li>
-          <li>
-            <Link href="/contact">Contact</Link>
-          </li>
+          {navLinks.map((navLink) => (
+            <li
+              className={activePath === navLink.path ? classes.linkActive : ''}
+              key={navLink.path}
+            >
+              <Link href={navLink.path}>{navLink.text}</Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,11 @@ import type { NextPage } from 'next'
 import type { Post } from '../type/post'
 import { Fragment } from 'react'
 import Hero from '../component/home/hero'
-import FeaturedPost from '../component/home/post-feature'
+import Article from '../component/home/article'
+import Repo from '../component/home/repo'
+import Contact from '../component/home/contact'
+import Summary from '../component/home/summary'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 import { getAllPosts } from '../lib/posts-util'
 
@@ -10,15 +14,19 @@ const Home: NextPage<{ posts: Array<Post> }> = (props) => {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPost posts={props.posts} />
+      <Summary />
+      <Article posts={props.posts} />
+      <Repo />
+      <Contact />
     </Fragment>
   )
 }
 
-export function getStaticProps() {
+export async function getStaticProps({ locale }) {
   const posts = getAllPosts()
   return {
     props: {
+      ...(await serverSideTranslations(locale, ['hero'])),
       posts,
     },
   }
